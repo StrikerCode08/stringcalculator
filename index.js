9e6f54b167a1660f80fca5a2f244bc160c6704eb
StrikerCode08 <akash@poshn.co>
2025-03-05 11:43:24 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,9 @@
+class StringCalculator {
+  constructor() {
+    this.defaultDelimiter = /,|\n/;
+  }
+  add(numberString) {
+    if (numberString === "") return 0;
+  }
+}
+module.exports = StringCalculator;
